7dc5f339ff47e69048e041c5a86efe6e607b1630
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2024-12-23 11:22:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+            Console.WriteLine("PID\tName");
+            foreach (Process process in processes)
+            {
+                Console.WriteLine($"{process.Id}\t{process.ProcessName}");
+            }
+        }
+    }
+}
